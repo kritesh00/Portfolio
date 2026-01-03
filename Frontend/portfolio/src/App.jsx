c254bea6f122ae58103b64a72aee1f472a36ba38
kritesh00 <kriteshbhattarai617@gmail.com>
2026-01-03 10:18:35 +0545

    Setup Portfolio Frontend

---
+++ Frontend/portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import Welcome from './Components/Welcome'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <Welcome />
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/portfolio/src/Components/Welcome.jsx
@@ -0,0 +1,29 @@
+
+function Welcome(){
+  
+    return(
+        <>
+             <div className="flex justify-center">
+          <img
+            src="https://user-images.githubusercontent.com/74038190/226190894-18e959ba-d458-4a94-ac44-790190f2a947.gif"
+            alt="Hero animation"
+            className="
+              w-80 md:w-96
+              rounded-2xl
+              border border-white/20
+              shadow-2xl
+              backdrop-blur-xl
+              transition
+              duration-500
+              hover:scale-105
+              
+            "
+          />
+        </div>
+        
+        </>
+
+    )
+    
+}
+export default Welcome;
