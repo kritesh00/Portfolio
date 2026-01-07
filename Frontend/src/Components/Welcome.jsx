52370579d6564b175bfa0a12b835e3ad1072c874
kritesh00 <kriteshbhattarai617@gmail.com>
2026-01-07 21:49:03 +0545

    Update

--- Frontend/src/Components/Welcome.jsx
+++ Frontend/src/Components/Welcome.jsx
@@ -4,7 +4,10 @@ function Welcome(){
         <>
         <div className='display-flex justify-center align-center  h-200 w-150 bg-white/10 rounded-xl border border-white/20 shadow-2xl backdrop-blur-xl mt-10 mx-auto p-6'>
         <div className='display-flex justify-center align-center flex-row'>
-        <div className='bg-red-500/20 m-3 p-4 rounded-lg h-50 w-50'></div>
+        <div className='bg-red-500/20 m-3 p-4 rounded-lg h-50 w-50'>
+        <img src="vite.svg" alt="" />
+        
+        </div>
         <div className='bg-blue-500/20 m-3 p-4 rounded-lg h-50 w-50'></div>
         <div className='bg-green-500/20 m-3 p-4 rounded-lg h-50 w-50'></div>
         </div>
