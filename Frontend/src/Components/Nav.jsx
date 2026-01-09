04721c6e5f5df6fe936161d5071f44075f0b5587
kritesh00 <kriteshbhattarai617@gmail.com>
2026-01-09 22:23:10 +0545

    Nav->class->className(Edited)

--- Frontend/src/Components/Nav.jsx
+++ Frontend/src/Components/Nav.jsx
@@ -1,12 +1,12 @@
 function Nav(){
     return(
         <>
-        <nav class="bg-[#061E29] text-white py-3 px-4 flex items-center justify-between">
-    <a class="font-bold text-xl tracking-tight" href="https://www.kriteshbhattrai.com.np">Kritesh</a>
-    <div class="flex items-center">
-        <a class="text-[#F3F4F4] px-4 py-2 leading-none rounded-full hover:text-[#5F9598]" href="#">Home</a>
-        <a class="text-[#F3F4F4] px-4 py-2 leading-none rounded-full hover:text-[#5F9598]" href="#about">About</a>
-        <a class="text-[#F3F4F4] px-4 py-2 leading-none rounded-full hover:text-[#5F9598]" href="#">Contact</a>
+        <nav className="bg-[#061E29] text-white py-3 px-4 flex items-center justify-between">
+    <a className="font-bold text-xl tracking-tight" href="https://www.kriteshbhattrai.com.np">Kritesh</a>
+    <div className="flex items-center">
+        <a className="text-[#F3F4F4] px-4 py-2 leading-none rounded-full hover:text-[#5F9598]" href="#">Home</a>
+        <a className="text-[#F3F4F4] px-4 py-2 leading-none rounded-full hover:text-[#5F9598]" href="#about">About</a>
+        <a className="text-[#F3F4F4] px-4 py-2 leading-none rounded-full hover:text-[#5F9598]" href="#">Contact</a>
     </div>
     </nav>
         </>
